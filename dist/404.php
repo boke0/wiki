f7f2e1b45fbd91acc2a8a26548ac963ae0985139
Boke0 <boke0@eugensite.com>
2019-10-17 18:56:06 +0900

    [fix] 編集画面の不具合を修正

--- dist/404.php
+++ dist/404.php
@@ -11,19 +11,29 @@ switch($seps[0]){
         header("Location: {$mtm->appinfo["application_root"]}setting.php");
         break;
     default:
-        $qd=[
-            "slug"=>$path
-        ];
-        foreach($_GET as $k=>$v) $qd[$k]=$v;
-        $qs=http_build_query($qd);
         switch(array_pop($seps)){
             case "edit":
+                $qd=[
+                    "slug"=>implode("/",$seps)
+                ];
+                foreach($_GET as $k=>$v) $qd[$k]=$v;
+                $qs=http_build_query($qd);
                 header("Location: {$mtm->appinfo["application_root"]}edit.php?$qs");
                 break;
             case "setting":
+                $qd=[
+                    "slug"=>implode("/",$seps)
+                ];
+                foreach($_GET as $k=>$v) $qd[$k]=$v;
+                $qs=http_build_query($qd);
                 header("Location: {$mtm->appinfo["application_root"]}item_setting.php?$qs");
                 break;
             default:
+                $qd=[
+                    "slug"=>$path
+                ];
+                foreach($_GET as $k=>$v) $qd[$k]=$v;
+                $qs=http_build_query($qd);
                 header("Location: {$mtm->appinfo["application_root"]}index.php?$qs");
                 break;
         }
